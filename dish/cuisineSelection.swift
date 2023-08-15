f998a2771215fcdd4e59aa278c84c11bc4953348
Alicia <42559357+aliciabao@users.noreply.github.com>
2023-08-15 14:14:33 -0400

    skeleton pick your cuisine pages all linked to recipeView

--- dish/cuisineSelection.swift
+++ dish/cuisineSelection.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct cuisineSelection: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack{
+                Text("Pick Your Cuisine")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                NavigationStack {
+                    NavigationLink(destination: recipeView()){
+                        Text("Italian")
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                    NavigationLink(destination: recipeView()){
+                        Text("Chinese")
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                    NavigationLink(destination: recipeView()){
+                        Text("French")
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                }
+            }
     }
 }
 
