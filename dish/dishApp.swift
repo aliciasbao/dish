a4db9fc80bfe2231ba8f771c89a299e701900130
Alicia <42559357+aliciabao@users.noreply.github.com>
2023-08-15 12:51:36 -0400

    Initial Commit

---
+++ dish/dishApp.swift
@@ -0,0 +1,17 @@
+//
+//  dishApp.swift
+//  dish
+//
+//  Created by Alicia Bao on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct dishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
