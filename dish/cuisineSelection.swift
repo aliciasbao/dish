854e2d4681fcb522f6dc8718b9a4e022b378c7da
Alicia <42559357+aliciabao@users.noreply.github.com>
2023-08-15 14:37:31 -0400

    half styled pick your cuisine

--- dish/cuisineSelection.swift
+++ dish/cuisineSelection.swift
@@ -13,6 +13,7 @@ struct cuisineSelection: View {
                 Text("Pick Your Cuisine")
                     .font(.title)
                     .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.2745, green: 0.4431, blue: 0.2667))
                 
                 NavigationStack {
                     NavigationLink(destination: recipeView()){
@@ -28,8 +29,13 @@ struct cuisineSelection: View {
                     }
                     .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
                 }
+                .padding()
+                
             }
+            .foregroundColor(Color(red: 0.2745, green: 0.4431, blue: 0.2667) /* #467144 */)
+            .background(Color(red: 0.9569, green: 0.9451, blue: 0.8824) /* cream */)
     }
+    
 }
 
 struct cuisineSelection_Previews: PreviewProvider {
